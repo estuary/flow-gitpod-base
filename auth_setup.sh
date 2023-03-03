7bcb9bcdeb3d237f9ed615104cdba422b68a69a0
Joseph Shearer <joseph@josephshearer.net>
2023-03-03 17:06:43 +0000

    Real auth setup

---
+++ auth_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+REFERSH_TOKEN=$(echo "$FLOW_REFRESH_TOKEN" | base64 -d)
+ACCESS_TOKEN_REQUEST=${REFERSH_TOKEN/\"id\"/\"refresh_token_id\"}
+ACCESS_TOKEN=$(
+    curl \
+        -XPOST \
+        -H "apikey: $FLOW_SUPABASE_ANON_TOKEN" \
+        -H "Content-Type: application/json" \
+        "https://$FLOW_SUPABASE_HOST)/rest/v1/rpc/generate_access_token" \
+        -d "$ACCESS_TOKEN_REQUEST"
+)
+MULTI_USE_REFRESH_TOKEN=$(
+    curl \
+        -XPOST \
+        -H "apikey: $FLOW_SUPABASE_ANON_TOKEN" \
+        -H "Authorization: Bearer $ACCESS_TOKEN" \
+        "https://$FLOW_SUPABASE_HOST/rest/rpc/generate_refresh_token" \
+        -d '{"multi_use": true, "valid_for":"7 days"}'\
+) 
+export FLOW_AUTH_TOKEN=$MULTI_USE_REFRESH_TOKEN
