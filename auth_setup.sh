f96ae42d395d1ee59972f70fc1109973c07ab6ab
Joseph Shearer <joseph@josephshearer.net>
2023-03-03 17:15:49 +0000

    fix a couple of typos

--- auth_setup.sh
+++ auth_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-REFERSH_TOKEN=$(echo "$FLOW_REFRESH_TOKEN" | base64 -d)
-ACCESS_TOKEN_REQUEST=${REFERSH_TOKEN/\"id\"/\"refresh_token_id\"}
+REFRESH_TOEN=$(echo "$FLOW_REFRESH_TOKEN" | base64 -d)
+ACCESS_TOKEN_REQUEST=${REFRESH_TOEN/\"id\"/\"refresh_token_id\"}
 ACCESS_TOKEN=$(
     curl \
         -XPOST \
         -H "apikey: $FLOW_SUPABASE_ANON_TOKEN" \
         -H "Content-Type: application/json" \
-        "https://$FLOW_SUPABASE_HOST)/rest/v1/rpc/generate_access_token" \
+        "https://$FLOW_SUPABASE_HOST/rest/v1/rpc/generate_access_token" \
         -d "$ACCESS_TOKEN_REQUEST"
 )
 MULTI_USE_REFRESH_TOKEN=$(
