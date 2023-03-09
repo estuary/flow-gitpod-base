686f228b77edbb14b54f8a13544fbc4c074f5702
Joseph Shearer <joseph@josephshearer.net>
2023-03-09 14:50:31 +0000

    refactor: Do JSON rewriting with `jq`

--- init.sh
+++ init.sh
@@ -2,14 +2,20 @@
 set -e # Tells bash to immediately exit on failure off a command
 
 REFRESH_TOKEN=$(echo "$FLOW_REFRESH_TOKEN" | base64 -d)
-ACCESS_TOKEN_REQUEST=${REFRESH_TOKEN/\"id\"/\"refresh_token_id\"}
+
+# Replace "id" with "refresh_token_id" because `create_refresh_token` returns
+# {"id" : "...", "secret" : "..."}
+# But `generate_access_token` expects
+# {"refresh_token_id" : "...", "secret" : "..."}
+ACCESS_TOKEN_REQUEST=$(echo "$REFRESH_TOKEN" | jq -c -r '. + {"refresh_token_id": .id}| del(.id)')
+
 ACCESS_TOKEN=$(
     curl \
         -XPOST \
         -H "apikey: $FLOW_SUPABASE_ANON_TOKEN" \
         -H "Content-Type: application/json" \
         "https://$FLOW_SUPABASE_HOST/rest/v1/rpc/generate_access_token" \
-        -d "$ACCESS_TOKEN_REQUEST" | jq -r '.access_token'
+        -d "$ACCESS_TOKEN_REQUEST" | jq -r -c '.access_token'
 )
 MULTI_USE_REFRESH_TOKEN=$(
     curl \
